f2943d026c1397e78ccb8b7685759c16618cce66
Ricky Kurniawan <ricky@firesnaker.com>
2021-09-07 21:21:23 +0700

    Added regex to escape double quotes with backslashes

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const TextLoader = require('./utils/TextLoader')
 const JSONLoader = require('./utils/JSONLoader')
 
 module.exports = (sample_text, sample_data, result_path) => {
-    const data = require(sample_data)
-    const sample_string = TextLoader(sample_text)
+    const data = require(sample_data) //json file
+    const sample_string = TextLoader(sample_text) //you are watching
 
-    const processed_string = Replacer(sample_string, data)
+	const reg = /"/g
+    const processed_string = Replacer(sample_string, data).replace(reg, '\\\"');
     const save_path = __dirname + '/' + result_path
-
+	
     SaveJSON(save_path, `{ "processed": "${ processed_string }" }`)
 
     const processed_json = JSONLoader(save_path)
